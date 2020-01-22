10772556f626509e27c3215292de1915345e76d3
EnrickLlari <59451048+EnrickLlari@users.noreply.github.com>
2020-01-22 17:49:14 +0100

    Create test_play.py

---
+++ game/test_play.py
@@ -0,0 +1,5 @@
+def test_init_play():
+    assert init_play() == {'n': 4, 'nb_cases_libres': 16,
+                            'tiles': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+    print("ok pour init_play")
+    
