03a13b91ca331958cec378383a3c0fad622c58fe
EnrickLlari <59451048+EnrickLlari@users.noreply.github.com>
2020-01-22 17:52:08 +0100

    Update test_play.py

--- game/test_play.py
+++ game/test_play.py
@@ -2,4 +2,31 @@ def test_init_play():
     assert init_play() == {'n': 4, 'nb_cases_libres': 16,
                             'tiles': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
     print("ok pour init_play")
+
+def test_is_game_over():
+    z = {'n': 4, 'nb_cases_libres': 0,
+            'tiles': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}
+    p = init_play()
+    assert is_game_over(p) == False
+    assert is_game_over(z) == True
+    print("ok pour is_game_over")
+
+def test_get_score():
+    z = {'n': 4, 'nb_cases_libres': 0,
+            'tiles': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}
+    p = init_play()
+    assert get_score(p) == 0
+    assert get_score(z) == 16
+    print("ok pour get_score")
+####################################
+#             PARTIE 3             #
+####################################
+def test_create_new_play():
+    plateau = init_play()  # On crée un plateau vide grâce à init_play
+    assert create_new_play() == {
+        'plateau': plateau,
+        'next_tile': {},
+        'score': 3
+    }
+    print("ok pour create_new_play")
     
