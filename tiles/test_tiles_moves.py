223e7d9f11b3c0a7aed93ec2b549c0cd6ccc5c77
EnrickLlari <59451048+EnrickLlari@users.noreply.github.com>
2020-01-23 13:54:20 +0100

    Add files via upload

--- tiles/test_tiles_moves.py
+++ tiles/test_tiles_moves.py
@@ -13,5 +13,25 @@ def test_get_nb_empty_rooms():
 ####################################
 #             PARTIE 2             #
 ####################################
+def test_get_next_alea_tiles():
+    p = {'n': 4, 'nb_cases_libres': 6, 'tiles': [6, 2, 3, 2, 0, 2, 6, 2, 0, 2, 2, 0, 1, 0, 0, 0],
+         'nombre_cases_libres': 5}
+    test = get_next_alea_tiles(p, 'init')
+    assert 1 in test is True
+    test = get_next_alea_tiles(p, 'encours')
+    assert 1 in test is False
+    print("ok pour get_next_alea_tiles")
 
+def test_put_next_tiles():
+    tiles = {0: {'val': '10', 'lig': 1, 'col': 0}, 'check': True}
+    p = {'n': 4, 'nb_cases_libres': 6, 'tiles': [6, 2, 3, 2, 0, 2, 6, 2, 0, 2, 2, 0, 1, 0, 0, 0],
+         'nombre_cases_libres': 5}
+    put_next_tiles(p,tiles)
+    assert p == {'n': 4, 'nb_cases_libres': 6, 'tiles': [6, 2, 3, 2, 10, 2, 6, 2, 0, 2, 2, 0, 1, 0, 0, 0],
+         'nombre_cases_libres': 4}
+    tiles = {0: {'val': '5', 'lig': 3, 'col': 3}, 'check': True}
+    put_next_tiles(p,tiles)
+    assert p == {'n': 4, 'nb_cases_libres': 6, 'tiles': [6, 2, 3, 2, 10, 2, 6, 2, 0, 2, 2, 0, 1, 0, 0, 5],
+         'nombre_cases_libres': 4}
+    print("ok pour put_next_tiles")
 
