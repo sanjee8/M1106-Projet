b0a5f956de7918aa7f7372dc2c7aba0d50fcb7d9
sanjee8 <59448693+sanjee8@users.noreply.github.com>
2020-01-23 22:43:20 +0100

    Update test_play.py

--- game/test_play.py
+++ game/test_play.py
@@ -8,7 +8,7 @@ def test_init_play():
 
 def test_is_game_over():
     z = {'n': 4, 'nb_cases_libres': 0,
-            'tiles': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}
+            'tiles': [1, 1, 2, 1, 1, 100, 1, 3, 1, 10, 1, 8, 1, 1, 3, 1]}
     p = init_play()
     assert is_game_over(p) == False
     assert is_game_over(z) == True
@@ -16,11 +16,12 @@ def test_is_game_over():
 
 def test_get_score():
     z = {'n': 4, 'nb_cases_libres': 0,
-            'tiles': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}
+            'tiles': [1, 2, 1, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1]}
     p = init_play()
     assert get_score(p) == 0
-    assert get_score(z) == 16
+    assert get_score(z) == 22
     print("ok pour get_score")
+
 ####################################
 #             PARTIE 3             #
 ####################################
